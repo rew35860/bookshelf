51aebdab8e202cc840a9a98eed2d15eefd1138a1
rew35860 <njengjir@uci.edu>
2021-07-02 01:49:52 -0700

    fix minor errors in createDirectory function

--- book.py
+++ book.py
@@ -7,21 +7,22 @@ from bs4 import BeautifulSoup
 class Book:
     def __init__(self, bookName, startCh, endCh):
         self.bookName = bookName
-        self.chapters = range(startCh, endCh)
-        self.homedir = str(Path.home())
+        self.chapters = range(startCh, endCh+1)
+        self.home = str(Path.home())
 
     # creating a direcotry to store books --> the folder will appear on desktop 
     def createDirectory(self): 
-        path = f'{home}/Desktop'
+        path = f'{self.home}/Desktop'
 
         if 'Book' not in os.listdir(path): 
             os.mkdir(f'{path}/Book')
-        else: 
-            path = f'{path}/Book'
-            
-            if self.bookName.upper() not in os.listdir(path)
-                os.mkdir(f'{path}/{self.bookName.upper()}')
-    
+        
+        path = f'{path}/Book'
+        
+        if self.bookName.upper() not in os.listdir(path):
+            os.mkdir(f'{path}/{self.bookName.upper()}')
+
+
     # parsing html pages and write it on a file 
     def createBook(self):
         try:
@@ -30,12 +31,12 @@ class Book:
             for number in self.chapters:
                 print(f'Currently Scraping Chapter: {number}\n')
 
-                title = '-'.join(self.title.lower().split())
+                title = '-'.join(self.bookName.lower().split())
                 html = requests.get(f"https://readwebnovels.net/novel/{title}/chapter-{number}/")
                 soup = BeautifulSoup(html.text, 'html.parser')
                 text = soup.findAll('div', {'class':'text-left'})
-
-                file1 = open(f'{home}/Desktop/chapter_{number}.txt', 'w')
+        
+                file1 = open(f'{self.home}/Desktop/Book/{self.bookName.upper()}/chapter_{number}.txt', 'w')
                 first = True
 
                 for line in text:
@@ -43,12 +44,14 @@ class Book:
                     for i in l: 
                         s = f'{i.text}\n\n'
                         if first: 
-                            file1.write('-'*200+'\n\n')
+                            file1.write('-'*100+'\n\n')
                             first = False
 
                         file1.write(s)
         except: 
-            return 'Invalid Title\n'
+            print('Invalid Title\n')
 
 
 if __name__ == "__main__":
+    read = Book("The Beginning After The End", 1, 2)
+    read.createBook()
